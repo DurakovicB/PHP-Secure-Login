43bc981a0d24fb2d5b1f229abfdaa6fd0d014166
Bilal Durakovic <bilal.durakovic@stu.ibu.edu.ba>
2024-05-18 15:36:59 +0200

    added /users route

--- routes/routes.php
+++ routes/routes.php
@@ -15,5 +15,35 @@ Flight::route('GET /login', function(){
     Flight::render('login.html');
 });
 
+include('rest/config.class.php');
+
+$servername = "localhost";
+$dbname = "secure_login";
+
+//$conn = new mysqli($servername, $mysql_username, $mysql_password, $dbname);
+$conn = new mysqli(Config::DB_HOST(), Config::DB_USERNAME(), Config::DB_PASSWORD(), $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+Flight::route('GET /users', function(){
+    global $conn;
+
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // User data found
+        $users = array();
+        while ($row = $result->fetch_assoc()) {
+            $users[] = $row;
+        }
+        Flight::json($users);
+    } else {
+        // No user data found
+        Flight::json(array('message' => 'No user data found.'));
+    }
+});
 
 Flight::start();
